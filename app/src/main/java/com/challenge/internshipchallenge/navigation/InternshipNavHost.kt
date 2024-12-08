9cd110f12d5c1f8a01fb237698bed5f4b25be80f
ImGaram <gim78014@gmail.com>
2024-12-08 18:27:27 +0900

    :sparkles: :: 메인, 회원가입 navHost 생성

---
+++ app/src/main/java/com/challenge/internshipchallenge/navigation/InternshipNavHost.kt
@@ -0,0 +1,48 @@
+package com.challenge.internshipchallenge.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.challenge.internshipchallenge.route.MainRoute
+import com.challenge.internshipchallenge.screen.MainScreen
+import com.challenge.internshipchallenge.screen.ProfileScreen
+import com.challenge.internshipchallenge.screen.SignInScreen
+import com.challenge.internshipchallenge.screen.SignUpScreen
+
+@Composable
+fun InternshipNavHost(
+    navController: NavHostController,
+    startDestination: String
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(MainRoute.Main.name) {
+            MainScreen(
+                navigateToSignUp = { navController.navigate(MainRoute.SignIn.name) },
+                navigateToProfile = { navController.navigate(MainRoute.Profile.name) }
+            )
+        }
+
+        composable(MainRoute.SignIn.name) {
+            SignInScreen(
+                navigateToProfile = { navController.navigate(MainRoute.Profile.name) },
+                navigateToMain = { navController.navigate(MainRoute.Main.name) }
+            )
+        }
+
+        composable(MainRoute.SignUp.name) {
+            SignUpScreen(
+                navigateToSignIn = { navController.navigate(MainRoute.SignUp.name) },
+            )
+        }
+
+        composable(MainRoute.Profile.name) {
+            ProfileScreen(
+                navigateToSignIn = { navController.navigate(MainRoute.SignIn.name) }
+            )
+        }
+    }
+}
